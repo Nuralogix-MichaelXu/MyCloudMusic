aaea6eaacbd042143da70acdf6baa04c535c28e1
徐庆庆 <xuqingqing@vaasplus.corp>
2022-08-21 16:24:20 +0800

    init project

---
+++ MyCloudMusic/MyCloudMusic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyCloudMusic
+//
+//  Created by xuqingqing on 2022/8/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(self)
+        print("人生苦短，我们只做好课~")
+        
+    }
+}
+
